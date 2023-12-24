b76188a7c90742e0b1d542c7fbf52f16fb75be64
SystemSculpt <systemsculpt@gmail.com>
2023-12-24 23:24:12 +0800

    Conforming to using leader, added file/inner file search methods

--- .config/nvim/lua/config/telescope_keymaps.lua
+++ .config/nvim/lua/config/telescope_keymaps.lua
@@ -1,24 +1,58 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
--- fr shows Telescope recent files
-keymap.set("n", "fr", ":Telescope oldfiles<CR>", opts)
+-- ###############################
+-- FZF / Find Files Keymaps
+-- ###############################
 
--- ffg shows Telescope ~/Desktop/gits files
+-- <Leader>ffg shows Telescope ~/Desktop/gits files
 keymap.set(
   "n",
-  "ffg",
+  "<Leader>ffg",
   ":lua require('telescope.builtin').find_files({ cwd = '~/Desktop/gits', hidden = true, file_ignore_patterns = { 'env/', 'node_modules/', '.git/', '__pycache__' } })<CR>",
   opts
 )
 
--- ffd shows Telescope ~/.config files
-keymap.set("n", "ffd", ":lua require('telescope.builtin').find_files({ cwd = '~/.config', hidden = true })<CR>", opts)
-
--- ffz shows Telescope ~/Documents/Obsidian/base_vault/ files
+-- <Leader>ffc shows Telescope ~/.config files
 keymap.set(
   "n",
-  "ffz",
+  "<Leader>ffc",
+  ":lua require('telescope.builtin').find_files({ cwd = '~/.config', hidden = true, file_ignore_patterns = { 'env/'} })<CR>",
+  opts
+)
+
+-- <Leader>ffo shows Telescope ~/Documents/Obsidian/base_vault/ files
+keymap.set(
+  "n",
+  "<Leader>ffo",
   ":lua require('telescope.builtin').find_files({ cwd = '~/Documents/Obsidian/base_vault/', hidden = true })<CR>",
+  opts
+)
+
+-- ###############################
+-- RG / Live Grep Keymaps
+-- ###############################
+
+-- <Leader>rgg shows Telescope ~/Desktop/gits files
+keymap.set(
+  "n",
+  "<Leader>rgg",
+  ":lua require('telescope.builtin').live_grep({ cwd = '~/Desktop/gits', hidden = true, file_ignore_patterns = { 'env/', 'node_modules/', '.git/', '__pycache__' } })<CR>",
+  opts
+)
+
+-- <Leader>rgc shows Telescope ~/.config files
+keymap.set(
+  "n",
+  "<Leader>rgc",
+  ":lua require('telescope.builtin').live_grep({ cwd = '~/.config', hidden = true, file_ignore_patterns = { 'env/'} })<CR>",
+  opts
+)
+
+-- <Leader>rgo shows Telescope ~/Documents/Obsidian/base_vault/ files
+keymap.set(
+  "n",
+  "<Leader>rgo",
+  ":lua require('telescope.builtin').live_grep({ cwd = '~/Documents/Obsidian/base_vault/', hidden = true })<CR>",
   opts
 )
